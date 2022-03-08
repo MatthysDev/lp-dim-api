0d3953eaf08ce54a9adedd07c5075f8c2d4daf61
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-03-08 18:55:56 +0100

    update content

---
+++ hello-world/src/app/matthys-ducrocq/matthys-ducrocq.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatthysDucrocqCoponents } from './matthys-ducrocq.components';
+
+@NgModule({
+  declarations: [MatthysDucrocqComponent],
+  imports: [CommonModule],
+  exports: [MatthysDucrocqComponent],
+})
+export class MatthysDucrocqModule {}
